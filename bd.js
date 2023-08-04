012716bb3b1593662eef223630bcd1790bfc8974
ViniiGomes <132698109+ViniiGomes@users.noreply.github.com>
2023-08-04 17:15:53 +0000

    att

---
+++ bd.js
@@ -0,0 +1,18 @@
+//banco de dados, arquivo 
+import pkg from "pg";
+const { Pool } = pkg;
+
+async function connect() {
+  const pool = new Pool({
+    connectionString: process.env.URL_BD,
+  });
+  return pool.connect();
+}
+
+async function selectUsuarios() {
+  const client = await connect();
+  const res = await client.query("SELECT * FROM usuario");
+  return res.rows;
+}
+
+export { selectUsuarios };
