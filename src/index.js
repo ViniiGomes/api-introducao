50f5aed4468e4beb95b38e8a9e93d3a3c51b9e8f
ViniiGomes <132698109+ViniiGomes@users.noreply.github.com>
2023-08-25 17:13:38 +0000

    att

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import express from "express";      // Requisição do pacote do express
+import dotenv from "dotenv";
+import roteadorUsuario from "./routes/usuario.js";
+const port = 3000;     
+             // Define a porta
+dotenv.config();
+
+app.use(express.json());
+
+app.listen(port, () => {            // Um socket para "escutar" as requisições
+  console.log(`Serviço escutando na porta:  ${port}`);
+});
+
+//index.js
+
+app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
+  res.json({
+    nome: "Vinícius Gomes Santos",      // Substitua pelo seu nome
+  });
+  console.log("Rota / solicitada");
+});
+app.use(roteadorUsuario);
+
+
+
